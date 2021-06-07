79eb2e52919e9195a69fd5954ff68eefa3474bcc
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-07 23:46:09 +0530

    Refactor:used java8 streams

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,19 +3,21 @@ package com.bridgelabz;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class AddressBookMain {
 	private static Scanner sc = new Scanner(System.in);
 	private static List<Person> personList = new ArrayList<Person>();
 
 	public static void main(String[] args) {
-		initiateAddressBook();
+		AddressBookMain addressBookMain = new AddressBookMain();
+		addressBookMain.initiateAddressBook();
 	}
 
 	/**
 	 * this method is used to initiate address book
 	 */
-	private static void initiateAddressBook() {
+	private void initiateAddressBook() {
 		boolean switcher = true;
 		do {
 			System.out.println("\n\tAddress Book Menu");
@@ -25,17 +27,14 @@ public class AddressBookMain {
 			System.out.println("\t\tEnter Q to Quit ");
 			System.out.print("\n\tPlease enter your choice: ");
 			char choice = sc.nextLine().toUpperCase().charAt(0);
-
 			while ((choice != 'A') && (choice != 'D') && (choice != 'M') && (choice != 'Q')) {
 				System.out.println("Invalid choice!  Please select (A)dd, (D)elete, (M)odify or (Q)uit: ");
 				choice = sc.nextLine().toUpperCase().charAt(0);
 			}
-
 			switch (choice) {
 			case 'A':
 				inputUserDetails("create");
 				break;
-
 			case 'D':
 				inputUserDetails("delete");
 				break;
@@ -48,10 +47,8 @@ public class AddressBookMain {
 				System.exit(0);
 				break;
 			default:
-
 			}
 		} while (switcher != false);
-
 	}
 
 	/**
@@ -60,7 +57,7 @@ public class AddressBookMain {
 	 * @param action
 	 * @return
 	 */
-	private static Person inputUserDetails(String action) {
+	private Person inputUserDetails(String action) {
 		if (action.equalsIgnoreCase("create")) {
 			Person newPerson = new Person();
 			System.out.println("\nTo add a person, follow the prompts.");
@@ -75,47 +72,21 @@ public class AddressBookMain {
 			System.out.println("\nTo edit a person, follow the prompts.");
 			System.out.println("\nEnter the first name of the person to edit");
 			String firstName = sc.nextLine();
-			/*
-			 * personList.stream().filter(person ->
-			 * person.getFirstName().equalsIgnoreCase(firstName)).findFirst().orElse(null);
-			 */
-			List<Person> filteredList = new ArrayList<Person>();
-			for (int i = 0; i < personList.size(); i++) {
-				if (personList.get(i).getFirstName().equalsIgnoreCase(firstName)) {
-					filteredList.add(personList.get(i));
-					break;
-				}
-			}
-			if (null != filteredList && filteredList.size() >= 1) {
-				Person person = filteredList.get(0);
-				inputCommonFields(person);
-
-				/*
-				 * personList.stream().map(p ->
-				 * p.getFirstName().equalsIgnoreCase(person.getFirstName()) ? person : p);
-				 */
-				for (int j = 0; j < personList.size(); j++) {
-					if (personList.get(j).getFirstName().equalsIgnoreCase(person.getFirstName())) {
-						personList.set(j, person);
-					}
-				}
-
+			Person editedPerson = personList.stream()
+					.filter(person -> person.getFirstName().equalsIgnoreCase(firstName)).findFirst().orElse(null);
+			if (null != editedPerson && null != editedPerson.getFirstName()) {
+				inputCommonFields(editedPerson);
+				personList = personList.stream()
+						.map(p -> p.getFirstName().equalsIgnoreCase(editedPerson.getFirstName()) ? editedPerson : p)
+						.collect(Collectors.toList());
 				System.out.println(personList.toString());
 			}
 		} else if (action.equalsIgnoreCase("delete")) {
 			System.out.println("\nTo delete a person, follow the prompts.");
 			System.out.println("\nEnter the first name of the person to be deleted");
 			String firstName = sc.nextLine();
-			/*
-			 * List<Person> filteredCollection = personList.stream() .filter(p ->
-			 * !(p.getFirstName().equalsIgnoreCase(firstName))).collect(Collectors.toList())
-			 * ;
-			 */
-			for (int k = 0; k < personList.size(); k++) {
-				if (personList.get(k).getFirstName().equalsIgnoreCase(firstName)) {
-					personList.remove(k);
-				}
-			}
+			personList = personList.stream().filter(p -> !(p.getFirstName().equalsIgnoreCase(firstName)))
+					.collect(Collectors.toList());
 			System.out.println(personList.toString());
 		}
 		return null;
@@ -126,24 +97,18 @@ public class AddressBookMain {
 	 * 
 	 * @param person
 	 */
-	private static void inputCommonFields(Person person) {
+	private void inputCommonFields(Person person) {
 		System.out.print("\nEnter Lastname: ");
 		person.setLastName(sc.nextLine());
-
 		System.out.print("Enter Address: ");
 		person.setAddress(sc.nextLine());
-
 		System.out.print("Enter City: ");
 		person.setCity(sc.nextLine());
-
 		System.out.print("Enter State: ");
 		person.setState(sc.nextLine());
-
 		System.out.print("Enter Zip: ");
 		person.setZip(sc.nextLine());
-
 		System.out.print("Enter Phone Number: ");
 		person.setPhoneNumber(sc.nextLine());
-
 	}
 }
