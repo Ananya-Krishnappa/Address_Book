ff3534caf99e51691bb6fd5c7d48be07f48d380b
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-07 20:47:29 +0530

    Added:create contacts

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,75 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		boolean switcher = true;
+		do {
+			System.out.println("\n\tAddress Book Menu");
+			System.out.println("\n\t\tEnter A to (A)dd Person ");
+			System.out.println("\t\tEnter D to (D)elete Person");
+			System.out.println("\t\tEnter M to (M)odify Person");
+			System.out.println("\t\tEnter Q to Quit");
+			System.out.print("\n\tPlease enter your choice: ");
+			char choice = sc.nextLine().toUpperCase().charAt(0);
+
+			while ((choice != 'A') && (choice != 'D') && (choice != 'M') && (choice != 'Q')) {
+				System.out.println("Invalid choice!  Please select (A)dd, (D)elete, (M)odify or (Q)uit: ");
+				choice = sc.nextLine().toUpperCase().charAt(0);
+			}
+
+			Person newPerson = new Person();
+			ArrayList<Person> personList = new ArrayList<Person>();
+
+			switch (choice) {
+			case 'A':
+				System.out.println("\nTo add a person, follow the prompts.");
+
+				System.out.print("\nEnter Fullname: ");
+				newPerson.setFirstName(sc.nextLine());
+
+				System.out.print("\nEnter Fullname: ");
+				newPerson.setLastName(sc.nextLine());
+
+				System.out.print("Enter Address: ");
+				newPerson.setAddress(sc.nextLine());
+
+				System.out.print("Enter City: ");
+				newPerson.setCity(sc.nextLine());
+
+				System.out.print("Enter State: ");
+				newPerson.setState(sc.nextLine());
+
+				System.out.print("Enter Zip: ");
+				newPerson.setZip(sc.nextLine());
+
+				System.out.print("Enter Phone Number: ");
+				newPerson.setPhoneNumber(sc.nextLine());
+
+				personList.add(newPerson);
+				System.out.println(personList.get(personList.size() - 1).toString());
+				System.out.println("\nYou have successfully added a new person!");
+
+				break;
+
+			case 'D':
+
+				break;
+			case 'M':
+
+				break;
+			case 'Q':
+				switcher = false;
+				sc.close();
+				System.exit(0);
+				break;
+			default:
+
+			}
+		} while (switcher != false);
+	}
+}
---
+++ src/com/bridgelabz/Person.java
@@ -0,0 +1,100 @@
+package com.bridgelabz;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+
+	public Person() {
+		super();
+	}
+
+	/**
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param address
+	 * @param city
+	 * @param state
+	 * @param zip
+	 * @param phoneNumber
+	 */
+	public Person(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
